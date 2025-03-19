a0d0d6a4920ca907592ced36247ff4f05db40671
Kingston <156565373+KingstonZhu0711@users.noreply.github.com>
2025-03-19 13:22:22 +0800

    Add files via upload

---
+++ Comp 2021- Object-oriented Programming/Assignment 1/Project/Source Code/test/hk/edu/polyu/comp/comp2021/tms/model/instructions/QuitExecutorTest.java
@@ -0,0 +1,39 @@
+package hk.edu.polyu.comp.comp2021.tms.model.instructions;
+
+import hk.edu.polyu.comp.comp2021.tms.model.CriterionRecorder;
+import hk.edu.polyu.comp.comp2021.tms.model.HistoryCommandRecorder;
+import hk.edu.polyu.comp.comp2021.tms.model.TaskRecorder;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuitExecutorTest {
+    private static QuitExecutor executor;
+    private static TaskRecorder taskRecorder;
+    private static HistoryCommandRecorder commandRecorder;
+    private static CriterionRecorder criterionRecorder;
+
+    @Before
+    public void setUp() {
+        executor = new QuitExecutor();
+        taskRecorder = new TaskRecorder();
+        commandRecorder = new HistoryCommandRecorder();
+        criterionRecorder = new CriterionRecorder();
+    }
+
+
+    @Test
+    public void testUndoInstruction() {
+        executor.undoInstruction(new String[]{}, taskRecorder, commandRecorder, criterionRecorder);
+        assertFalse(executor.getStatus());
+    }
+
+    @Test
+    public void testGetStatus() {
+        assertFalse(executor.getStatus());
+
+    }
+
+}
